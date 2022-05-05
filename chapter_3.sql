58487b35b50d6aff6b9b347fbc46e6417c7eb579
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-05-05 18:02:59 -0300

    Update chapter_3.sql

--- chapter_3.sql
+++ chapter_3.sql
@@ -46,3 +46,37 @@ FROM invoiceline
   -- Complete the join to the invoice table
 LEFT JOIN invoice
 ON invoiceline.invoice_id = invoice.invoice_id;
+
+------------------------------------------------------------------------------------------------------------------
+
+
+SELECT 
+  album.album_id,
+  title,
+  album.artist_id,
+  artist.name as artist
+FROM album
+INNER JOIN artist ON album.artist_id = artist.artist_id
+-- Perform the correct join type to return matches or NULLS from the track table
+RIGHT JOIN track on album.album_id = track.album_id
+WHERE album.album_id IN (213,214)
+
+
+------------------------------------------------------------------------------------------------------------------
+
+SELECT 
+  album_id AS ID,
+  title AS description,
+  'Album' AS Source
+  -- Complete the FROM statement
+  FROM album
+ -- Combine the result set using the relevant keyword
+UNION 
+SELECT 
+  artist_id AS ID,
+  name AS description,
+  'Artist'  AS Source
+  -- Complete the FROM statement
+FROM artist;
+
+------------------------------------------------------------------------------------------------------------------
