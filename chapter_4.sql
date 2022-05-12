99018ebf45d182a5e2d2869c829502a34289d982
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-05-12 20:56:08 -0300

    Update chapter_4.sql

--- chapter_4.sql
+++ chapter_4.sql
@@ -1,1 +1,70 @@
+-- Create the table
+CREATE TABLE results (
+	-- Create track column
+	track VARCHAR(200),
+    -- Create artist column
+	artist VARCHAR(120),
+    -- Create album column
+	album VARCHAR(160),
+	-- Create track_length_mins
+	track_length_mins INT,
+	);
 
+-- Select all columns from the table
+SELECT 
+   track, 
+  artist, 
+  album, 
+  track_length_mins 
+FROM 
+  results;
+  
+----------------------------------------------------------------------------------------------------------------------
+-- Create the table
+CREATE TABLE tracks(
+  -- Create track column
+  track VARCHAR(200), 
+  -- Create album column
+  album VARCHAR(160), 
+  -- Create track_length_mins column
+  track_length_mins INT
+);
+-- Complete the statement to enter the data to the table         
+INSERT INTO tracks
+-- Specify the destination columns
+(track, album, track_length_mins)
+-- Insert the appropriate values for track, album and track length
+VALUES
+  ('Basket Case', 'Dookie', 3)
+-- Select all columns from the new table
+SELECT 
+  *
+FROM 
+  tracks;
+  
+  
+----------------------------------------------------------------------------------------------------------------------
+
+-- Select the album
+SELECT 
+  title 
+FROM 
+  album 
+WHERE 
+  album_id = 213;
+-- UPDATE the title of the album
+UPDATE 
+  album 
+SET 
+  title = 'Pure Cult: The Best Of The Cult' 
+WHERE 
+  album_id = 213;
+-- Run the query again
+SELECT 
+  title 
+FROM 
+  album
+WHERE 
+  album_id = 213;
+  
+----------------------------------------------------------------------------------------------------------------------
