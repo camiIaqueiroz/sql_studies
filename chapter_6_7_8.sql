2eb79fd0631017aff03f19f496b7bb179e532d80
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-05-30 19:34:24 -0300

    Update chapter_6_7_8.sql

--- chapter_6_7_8.sql
+++ chapter_6_7_8.sql
@@ -153,5 +153,71 @@ JOIN BloodGlucoseRandom b
 ON a.BloodGlucoseRandom = b.MaxGlucose
 -------------------------------------------------------------------------------------------------
 Create a windows data grouping
-
+-------------------------------------------------------------------------------------------------
 OVER (PARTITION BY SalesYear ORDER BY SalesYear) 
+
+SELECT OrderID, TerritoryName, 
+       -- Total price for each partition
+       SUM(OrderPrice)
+       -- Create the window and partitions
+       OVER (PARTITION BY TerritoryName) AS TotalPrice
+FROM Orders
+-------------------------------------------------------------------------------------------------
+SELECT OrderID, TerritoryName, 
+       -- Number of rows per partition
+       COUNT(OrderID)
+       -- Create the window and partitions
+      OVER(PARTITION BY TerritoryName) AS TotalOrders
+FROM Orders
+-------------------------------------------------------------------------------------------------
+SELECT TerritoryName, OrderDate, 
+       -- Select the first value in each partition
+        FIRST_VALUE(OrderDate) 
+       -- Create the partitions and arrange the rows
+       OVER(PARTITION BY TerritoryName ORDER BY OrderDate) AS FirstOrder
+FROM Orders
+-------------------------------------------------------------------------------------------------
+SELECT TerritoryName, OrderDate, 
+       -- Specify the previous OrderDate in the window
+       LAG(OrderDate) 
+       -- Over the window, partition by territory & order by order date
+       OVER(PARTITION BY TerritoryName ORDER BY OrderDate) AS PreviousOrder,
+       -- Specify the next OrderDate in the window
+       LEAD(OrderDate) 
+       OVER(PARTITION BY TerritoryName ORDER BY OrderDate) AS NextOrder
+       -- Create the partitions and arrange the rows
+      
+FROM Orders
+-------------------------------------------------------------------------------------------------
+SELECT TerritoryName, OrderDate, 
+       -- Create a running total
+       SUM(OrderPrice) 
+       -- Create the partitions and arrange the rows
+       OVER(PARTITION BY TerritoryName ORDER BY OrderDate) AS TerritoryTotal	  
+FROM Orders
+-------------------------------------------------------------------------------------------------
+SELECT TerritoryName, OrderDate, 
+       -- Assign a row number
+       ROW_NUMBER()
+       -- Create the partitions and arrange the rows
+       OVER(PARTITION BY TerritoryName ORDER BY OrderDate) AS OrderCount
+FROM Orders
+-------------------------------------------------------------------------------------------------
+SELECT OrderDate, TerritoryName, 
+       -- Calculate the standard deviation
+	   STDEV(OrderPrice) 
+       OVER(PARTITION BY TerritoryName ORDER BY OrderDate) AS StdDevPrice	  
+FROM Orders
+-------------------------------------------------------------------------------------------------
+-- Create a CTE Called ModePrice which contains two columns
+WITH ModePrice (OrderPrice, UnitPriceFrequency)
+AS
+(
+	SELECT OrderPrice, 
+	ROW_NUMBER() 
+	OVER(PARTITION BY OrderPrice ORDER BY OrderPrice) AS UnitPriceFrequency
+	FROM Orders 
+)
+
+-- Select everything from the CTE
+SELECT * FROM ModePrice
