1500767a8012bed68390487db7d1aba895776d41
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-05-30 19:37:05 -0300

    Update chapter_6_7_8.sql

--- chapter_6_7_8.sql
+++ chapter_6_7_8.sql
@@ -221,3 +221,19 @@ AS
 
 -- Select everything from the CTE
 SELECT * FROM ModePrice
+-------------------------------------------------------------------------------------------------
+-- CTE from the previous exercise
+WITH ModePrice (OrderPrice, UnitPriceFrequency)
+AS
+(
+	SELECT OrderPrice,
+	ROW_NUMBER() 
+    OVER (PARTITION BY OrderPrice ORDER BY OrderPrice) AS UnitPriceFrequency
+	FROM Orders
+)
+
+-- Select the order price from the CTE
+SELECT OrderPrice AS ModeOrderPrice
+FROM ModePrice
+-- Select the maximum UnitPriceFrequency from the CTE
+WHERE UnitPriceFrequency IN (SELECT MAX(UnitPriceFrequency) FROM ModePrice)
