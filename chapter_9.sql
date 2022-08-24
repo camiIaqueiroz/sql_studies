1e1215959fba5a57999d203366f05ecd966ecf23
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-08-24 17:34:56 -0300

    Update chapter_9.sql

--- chapter_9.sql
+++ chapter_9.sql
@@ -3,3 +3,28 @@ SELECT table_name
 FROM information_schema.tables
 -- Specify the correct table_schema value
 WHERE table_schema = 'public';
+
+-- Query the right table in information_schema to get columns
+SELECT column_name, data_type 
+FROM information_schema.columns
+WHERE table_name = 'university_professors' AND table_schema = 'public';
+
+-- Query the first five rows of our table
+SELECT * 
+FROM university_professors 
+LIMIT 5;
+
+-- Create a table for the universities entity type
+CREATE TABLE universities (
+    university_shortname text,
+    university text,
+    university_city text
+ );
+
+
+
+
+
+-- Print the contents of this table
+SELECT * 
+FROM universities
